792cd65388ca79a3170504b1c91ced65cf8691ef
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-06-30 17:32:08 +0300

    refactor : remove unused files

--- src/content/home/confirmationOptions.ts
+++
@@ -1,6 +0,0 @@
-const confirmationOptions = [
-  { value: "はい", label: "はい" }, // Yes
-  { value: "いいえ", label: "いいえ" }, // No
-];
-
-export default confirmationOptions
--- src/content/home/usageOptions.ts
+++
@@ -1,6 +0,0 @@
-const usageOptions = [
-  { value: "事業（個人事業者または法人)", label: "事業（個人事業者または法人)" }, // Business
-  { value: "個人で使用", label: "個人で使用" }, // Personal
-];
-
-export default usageOptions
