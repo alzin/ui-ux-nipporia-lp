eb7ed3d9b41ce4e6bb4c03f67eac8eb1c46c9480
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-06-30 14:42:02 +0300

    feat(header): make a logo clickable

--- src/components/common/sections/Header.tsx
+++ src/components/common/sections/Header.tsx
@@ -8,9 +8,9 @@ export default function Header() {
   return (
     <nav className="fixed top-0 w-full bg-dark/95 backdrop-blur-[20px] border-b border-white/10 z-[1000] transition-all duration-300 ease-in-out">
       <div className="max-w-[1200px] mx-auto px-8 py-4 flex justify-between items-center">
-        <div className="text-[1.5rem] font-bold bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text bg-[length:200%_200%] animate-gradientShift hover:scale-110 transition-transform duration-300 ease-in-out">
+        <Link href={'/'} className="text-[1.5rem] font-bold bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text bg-[length:200%_200%] animate-gradientShift hover:scale-110 transition-transform duration-300 ease-in-out">
           WebTransform
-        </div>
+        </Link>
         <ul
           className={`hidden md:flex gap-8 ${isMenuOpen ? "block" : "hidden"}`}
         >
