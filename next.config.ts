b071868c83c67dd5557e830979c06d0551d9b130
Rezkaudi2002 <rezkaudi2002@gmail.com>
2026-04-29 10:55:52 +0300

    chore: enable standalone output mode in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["localhost:3000", "192.168.1.123:3000", "192.168.1.123"],
-  // output: 'standalone',
+  output: 'standalone',
   productionBrowserSourceMaps: false,
   turbopack: {
     rules: {
